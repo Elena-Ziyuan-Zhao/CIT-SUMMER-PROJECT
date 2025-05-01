fd8f9b991b5f2b18279b9143d4436897cc66b65b
Marcus Bonacci <marcusbonacci+github@gmail.com>
2025-04-30 18:27:35 -0700

    Added rating datamodel [UNTESTED] and a .gitignore file.

---
+++ models/rating.py
@@ -0,0 +1,11 @@
+from db import db
+from datetime import datetime
+class Rating(db.Model):
+    __tablename__ = "rating" 
+
+    id = db.mapped_column(db.Integer, primary_key = True)
+    secret_id = db.mapped_column(db.ForeignKey("secrets.id"))
+    user_id = db.mapped_column(db.ForeignKey('users.id'))
+    rating = db.mapped_column(db.Integer, default = 0)
+
+    created_date = db.mapped_column(db.DateTime, default = datetime.now())
