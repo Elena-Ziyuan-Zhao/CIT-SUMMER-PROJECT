4f9e1f1620eb36b43cdb8f46c46d2d6368e1cdb3
ZigZag <135251219+Skylar-ZiyuanZhao@users.noreply.github.com>
2025-05-22 13:56:59 -0700

    fix the bug that user can go back to rating page and rate more than one times and only allow implement user roles to deny access for non-admin users

--- tests/test_user.py
+++ tests/test_user.py
@@ -14,7 +14,7 @@ class TestUserModel(unittest.TestCase):
             first_name="Langston",
             second_name="Chau",
             username="langstonch",
-            user_type="regular",
+            role="regular",
             email="langstonch@gmail.com",
             password="securepassword123",
             created_date=custom_date  
@@ -24,12 +24,12 @@ class TestUserModel(unittest.TestCase):
         self.assertEqual(self.user.first_name, "Langston")
         self.assertEqual(self.user.second_name, "Chau")
         self.assertEqual(self.user.username, "langstonch")
-        self.assertEqual(self.user.user_type, "regular")
+        self.assertEqual(self.user.role, "regular")
         self.assertEqual(self.user.email, "langstonch@gmail.com")
         self.assertEqual(self.user.password, "securepassword123")
         self.assertIsInstance(self.user.created_date, datetime)
 
-    def test_default_user_type(self):
+    def test_default_role(self):
         user = User(
             first_name="Someone",
             second_name="Name",
@@ -37,9 +37,9 @@ class TestUserModel(unittest.TestCase):
             email="soname@example.com",
             password="anothersecurepassword"
         )
-        if user.user_type is None:
-            user.user_type = "regular"
-        self.assertEqual(user.user_type, "regular")
+        if user.role is None:
+            user.role = "regular"
+        self.assertEqual(user.role, "regular")
 
     def test_relationships(self):
         self.assertEqual(self.user.secrets, [])
